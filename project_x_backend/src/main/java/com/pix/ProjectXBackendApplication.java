cfd048bc020afbd0ef109649c13d9b224afc7b56
HanaPark1 <81073857+HanaPark1@users.noreply.github.com>
2025-09-15 10:22:42 +0900

    chore: Initial commit

---
+++ project_x_backend/src/main/java/com/pix/ProjectXBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectXBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectXBackendApplication.class, args);
+	}
+
+}
